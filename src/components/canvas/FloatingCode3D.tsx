1515b1d8d3e35e13aefb665f5279cd171c1c0289
Manoj Kumar <vandanamanojkumar13@gmail.com>
2026-07-17 15:01:07 +0530

    Fix 3D canvas pointer events blocking clicks, improve certification card styling

--- src/components/canvas/FloatingCode3D.tsx
+++ src/components/canvas/FloatingCode3D.tsx
@@ -80,7 +80,7 @@ function Scene() {
 export function FloatingCode3D() {
   return (
     <div className="fixed inset-0 pointer-events-none z-0" style={{ perspective: "1000px" }}>
-      <Canvas camera={{ position: [0, 0, 8], fov: 45 }} style={{ pointerEvents: "none" as const }}>
+      <Canvas camera={{ position: [0, 0, 8], fov: 45 }} onCreated={(s) => { s.gl.domElement.style.pointerEvents = "none"; }}>
         <Scene />
       </Canvas>
     </div>
